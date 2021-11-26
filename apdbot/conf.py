9de17f8cffd1e7afc96be64e64e986751d5e7c23
Show Flix <83741010+showflix@users.noreply.github.com>
2021-11-26 09:41:33 +0530

    Update conf.py

--- apdbot/conf.py
+++ apdbot/conf.py
@@ -2,8 +2,8 @@ from apdbot.sample_con import Config
 #Fill your all data here Do not Edit other Files
 
 class Config(Config):
-    BOT_TOKEN= ""
+    BOT_TOKEN= "2101378398:AAHCzyeMF6kvGEHbX1QEONqEfdH6GV1e5IY"
     APP_ID = 11792628
     API_HASH = "752cf9e02d21c275c2728954c62a20e4"
-    OWNER_ID = ""
+    OWNER_ID = "2093321027"
     AUTH = [2093321027, -606642980]
